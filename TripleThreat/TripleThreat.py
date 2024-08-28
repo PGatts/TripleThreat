7ab0852b629676365218f416fff6ef0427f74e7c
Param Gattupalli <paramgattupalli@gmail.com>
2024-08-28 14:55:11 -0400

    Used Flask to convert to a web application

--- TripleThreat/TripleThreat.py
+++ TripleThreat/TripleThreat.py
@@ -3,12 +3,14 @@
 # Created 09/16/2023
 # Last modified: 09/16/2023
 import random
-import pandas as pd
-from tabulate import tabulate
 import requests
 from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
+import ast
 
-def get_team_all_time(team_code): #returns a set of all players that played for a given team
+app = Flask(__name__)
+def get_team_all_time(team_code):
+    """returns a set of all players that played for a given team"""
     url="https://www.basketball-reference.com/teams/" + team_code + "/players.html"
     page= requests.get(url)
     if page.status_code == 200:
@@ -23,33 +25,54 @@ def get_team_all_time(team_code): #returns a set of all players that played for 
             if cols[0].text.strip().isdigit():#skips rows that aren't players
                 player_list.add(cols[1].text.strip().upper())
         return player_list
-    else:
+    else:#if the request fails
+        print(url)
         return set()
+
+def get_image(player_name):
+    """Returns an image address for the given player"""
+    player_lower = player_name.lower()
+    space_index = player_lower.find(' ')
+    last_name_first_letter = player_lower[space_index + 1]
+    i = 1
+    while i < 5:
+        url = 'https://basketball-reference.com/players/' + last_name_first_letter + '/' + player_lower[space_index + 1: space_index + 6] + player_lower[:2] + '0' + str(i) + '.html'
+        page = requests.get(url)
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.content, 'html.parser')
+            name = soup.find('h1')
+            if name.text.strip().lower() != player_lower: #go to the next page if there is another player with the same name
+                i += 1
+                continue
+            picture = soup.find('img', attrs={'itemscope':'image'})
+            image_link = picture['src']
+            return image_link
 
 # Generates 6 random, non-repeating teams from list of NBA teams
 def generate_teams():
-    """Returns a list of 6 unique teams"""
+    """Returns a list of image addresses for 6 unique teams' logos"""
     team_list = ['ATL', 'BOS', 'NJN', 'CHA', 'CHI', 'CLE', 'DAL', 'DEN', 'DET', 'GSW', 'HOU', 'IND', 'LAC', 'LAL', 'MEM', 'MIA', 'MIL', 'MIN', 'NOH', 'NYK', 'OKC', 'ORL', 'PHI', 'PHO', 'POR', 'SAC', 'SAS', 'TOR', 'UTA', 'WAS']
     return_list = []
+    index_set = set()
     while len(return_list) < 6:
         rand_index = random.randint(0,29)
-        if team_list[rand_index] not in return_list:
-            return_list.append(team_list[rand_index])
+        if rand_index not in index_set: #ensure that the index hasn't already been used
+            index_set.add(rand_index)
+            return_list.append("https://cdn.ssref.net/req/202408212/tlogo/bbr/" + team_list[rand_index] + ".png")
     return return_list
 
 # Determine whether the game is over or not, returns True if game is over
 def check_game_status(board):
-    """Checks if game is over. Returns True if finished, False otherwise."""
+    """Checks if game is over. Returns the winner if finished, None otherwise."""
     # Check each column & row
     for i in range(3):
-        if board[i][0] == board[i][1] == board[i][2] or board[0][i] == board[1][i] == board[2][i]:
-            print(board[i][i], "WINS!")
-            return True
+        if board[i][0][0] == board[i][1][0] == board[i][2][0] or board[0][i][0] == board[1][i][0] == board[2][i][0]:
+            return board[i][i][0]
     # Check the diagonals
-    if board[0][0] == board[1][1] == board[2][2] or board[0][2] == board[1][1] == board[2][0]:
-        print(board[1][1], "WINS!")
-        return True
-    return False
+    if board[0][0][0] == board[1][1][0] == board[2][2][0] or board[0][2][0] == board[1][1][0] == board[2][0][0]:
+        return board[1][1][0]
+    return None
+
 
 # Get array indices based on selected box
 def get_indices(box_number):
@@ -65,84 +88,82 @@ def get_indices(box_number):
     return row,col
 
 
-t_list = generate_teams()
-columns  = [t_list[0],t_list[1],t_list[2]]
-rows = [t_list[3],t_list[4],t_list[5]]
-answers = [['1','2','3'],['4','5','6'],['7','8','9']]
-end, xturn = False, True
+@app.route('/', methods=['GET', 'POST'])
+def game():
+    if request.method == "GET": #For the first time
+        t_list = generate_teams()
+        columns = t_list[:3]
+        rows = t_list[3:]
 
-# Create arrays of players from each team
-col0players= get_team_all_time(columns[0])
-col1players= get_team_all_time(columns[1])
-col2players= get_team_all_time(columns[2])
-column_players = [col0players,col1players,col2players]
+        # Create arrays of players from each team
+        col0players = get_team_all_time(columns[0][46:49])
+        col1players = get_team_all_time(columns[1][46:49])
+        col2players = get_team_all_time(columns[2][46:49])
+        column_players = [col0players, col1players, col2players]
 
-row0players= get_team_all_time(rows[0])
-row1players= get_team_all_time(rows[1])
-row2players= get_team_all_time(rows[2])
-row_players = [row0players,row1players,row2players]
+        row0players = get_team_all_time(rows[0][46:49])
+        row1players = get_team_all_time(rows[1][46:49])
+        row2players = get_team_all_time(rows[2][46:49])
+        row_players = [row0players, row1players, row2players]
 
-players = []
-for row in range(3):
-    for col in range(3):
-        players.append(row_players[row].intersection(column_players[col]))
+        #create a list of lists that contains correct answers for each square
+        players = []
+        for row in range(3):
+            for col in range(3):
+                players.append(list(row_players[row].intersection(column_players[col])))
 
-del col0players, col1players, col2players, row0players, row1players, row2players, column_players, row_players
+        del col0players, col1players, col2players, row0players, row1players, row2players, row_players, column_players
 
-def print_board():
-    table_headers = ['',columns[0],columns[1],columns[2]]
-    table = [[rows[0], answers[0][0],answers[0][1],answers[0][2]], [rows[1],answers[1][0],answers[1][1],answers[1][2]], [rows[2],answers[2][0],answers[2][1],answers[2][2]]]
+        game_data = {
+            'columns': columns,
+            'rows': rows,
+            'answers': [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']],
+            'players': players,
+            'xturn': True,
+            'filled_boxes': [],
+            'winner': None
+        }
 
-    print(tabulate(table,headers = table_headers,numalign='center',stralign='center'))
+        print(game_data)
+        return render_template('game.html', game_data=game_data)
+    else: #name was submitted
+        box = int(request.form['box'])
+        player = request.form['player'].upper().strip()
+        row, col = get_indices(box)
+
+        #retrieve the game state from the form
+        game_data = {
+            'columns': ast.literal_eval(request.form.getlist('columns')[0]),
+            'rows': ast.literal_eval(request.form.getlist('rows')[0]),
+            'answers': ast.literal_eval(request.form.getlist('answers')[0]),
+            'players': ast.literal_eval(request.form.getlist('players')[0]),
+            'xturn': request.form.get('xturn'),
+            'filled_boxes': ast.literal_eval(request.form.getlist('filled_boxes')[0]),
+            'winner': request.form.get('winner')
+        }
+
+        #handle the user's guess for the given box
+        if box not in game_data['filled_boxes']:
+            if player in game_data['players'][box - 1]:
+                game_data['answers'][row][col] = 'X' if game_data['xturn'] == 'True' else 'O'
+                game_data['answers'][row][col] += get_image(player)
+                game_data['filled_boxes'].append(box)
+
+                #check if the game is over or not
+                winner = check_game_status(game_data['answers'])
+                if winner:
+                    game_data['winner'] = winner
+                elif len(game_data['filled_boxes']) >= 9:
+                    game_data['winner'] = 'Draw'
+            #change turns
+            game_data['xturn'] = 'False' if game_data['xturn'] == 'True' else 'True'
+
+
+        print(game_data)
+        return render_template('game.html', game_data=game_data)
 
 
 
-filled_boxes = set()
-while not end and len(filled_boxes) < 9:
-    # Print current board
-    print_board()
-    box = input("Which square would you like to fill in? (Enter -999 to exit): ")
 
-    # Check that the selection is valid
-    try:
-        # Convert to int, raises ValueError if unable to
-        box = int(box)
-        available_spot = True
-        if box == -999:
-            break
-        # Ensure that box is 1-9
-        if not 0 < box < 10:
-            raise ValueError
-        # Ensure that selected box hasn't been replaced already
-        if box in filled_boxes:
-            available_spot = False
-        # Print error and retry if invalid input
-        if not available_spot:
-            print("That square has already been filled. Try Again!\n")
-            continue
-    # Print error and retry if invalid input
-    except ValueError:
-        print('Please enter an integer 1-9.\n')
-        continue
-
-    row, col = get_indices(box)
-    player = input("Name a player who played for both " + rows[row] + " and " + columns[col] + ": ")
-
-    #check if valid answer
-    if player.upper() in players[box - 1]:
-        if xturn:
-            answers[row][col] = 'x'
-        else:
-            answers[row][col] = 'o'
-        print("You got one. Next player's turn.")
-        filled_boxes.add(box)
-    else:
-        print(player.capitalize(), "did not play for both teams. Next player's turn.")
-
-    #change player turn after guess
-    xturn = not xturn
-    end = check_game_status(answers)
-
-if len(filled_boxes) > 8:
-    print("IT'S A DRAW!")
-print("GAME OVER!")
+if __name__ == '__main__':
+    app.run(debug=True)
