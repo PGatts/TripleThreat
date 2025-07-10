aee439415c47e194e1d6fc7f717b863a23a0f259
Param Gattupalli <paramgattupalli@gmail.com>
2025-07-10 13:16:14 -0400

    Works

--- TripleThreat/TripleThreat.py
+++ TripleThreat/TripleThreat.py
@@ -5,10 +5,15 @@
 import random
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import ast
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
+app.secret_key = os.getenv('SESSION_KEY')
+
 def get_team_all_time(team_code):
     """returns a set of all players that played for a given team"""
     url="https://www.basketball-reference.com/teams/" + team_code + "/players.html"
@@ -37,6 +42,7 @@ def get_image(player_name):
     i = 1
     while i < 5:
         url = 'https://basketball-reference.com/players/' + last_name_first_letter + '/' + player_lower[space_index + 1: space_index + 6] + player_lower[:2] + '0' + str(i) + '.html'
+        print(url)
         page = requests.get(url)
         if page.status_code == 200:
             soup = BeautifulSoup(page.content, 'html.parser')
@@ -72,10 +78,10 @@ def check_game_status(board):
     # Check each column & row
     for i in range(3):
         if board[i][0][0] == board[i][1][0] == board[i][2][0] or board[0][i][0] == board[1][i][0] == board[2][i][0]:
-            return board[i][i][0]
+            return 'RED' if board[i][i][0] == 'X' else 'BLUE'
     # Check the diagonals
     if board[0][0][0] == board[1][1][0] == board[2][2][0] or board[0][2][0] == board[1][1][0] == board[2][0][0]:
-        return board[1][1][0]
+        return 'RED' if board[1][1][0] == 'X' else 'BLUE'
     return None
 
 
@@ -95,30 +101,36 @@ def get_indices(box_number):
 
 @app.route('/', methods=['GET', 'POST'])
 def game():
-    if request.method == "GET": #For the first time
-        t_list = generate_teams()
-        columns = t_list[:3]
-        rows = t_list[3:]
-
-        # Create arrays of players from each team
-        col0players = get_team_all_time(columns[0][46:49])
-        col1players = get_team_all_time(columns[1][46:49])
-        col2players = get_team_all_time(columns[2][46:49])
-        column_players = [col0players, col1players, col2players]
-
-        row0players = get_team_all_time(rows[0][46:49])
-        row1players = get_team_all_time(rows[1][46:49])
-        row2players = get_team_all_time(rows[2][46:49])
-        row_players = [row0players, row1players, row2players]
-
-        #create a list of lists that contains correct answers for each square
-        players = []
-        for row in range(3):
-            for col in range(3):
-                players.append(list(row_players[row].intersection(column_players[col])))
-
-        del col0players, col1players, col2players, row0players, row1players, row2players, row_players, column_players
-
+    if request.method == "GET":
+        # Check for new teams request
+        if request.args.get('new_teams') == '1':
+            session.pop('columns', None)
+            session.pop('rows', None)
+            session.pop('players', None)
+        if 'columns' in session and 'rows' in session and 'players' in session:
+            columns = session['columns']
+            rows = session['rows']
+            players = session['players']
+        else:
+            t_list = generate_teams()
+            columns = t_list[:3]
+            rows = t_list[3:]
+            # Create arrays of players from each team
+            col0players = get_team_all_time(columns[0][46:49])
+            col1players = get_team_all_time(columns[1][46:49])
+            col2players = get_team_all_time(columns[2][46:49])
+            column_players = [col0players, col1players, col2players]
+            row0players = get_team_all_time(rows[0][46:49])
+            row1players = get_team_all_time(rows[1][46:49])
+            row2players = get_team_all_time(rows[2][46:49])
+            row_players = [row0players, row1players, row2players]
+            players = []
+            for row in range(3):
+                for col in range(3):
+                    players.append(list(row_players[row].intersection(column_players[col])))
+            session['columns'] = columns
+            session['rows'] = rows
+            session['players'] = players
         game_data = {
             'columns': columns,
             'rows': rows,
@@ -128,47 +140,48 @@ def game():
             'filled_boxes': [],
             'winner': None
         }
-
-        print(game_data)
         return render_template('game.html', game_data=game_data)
-    else: #name was submitted
-        box = int(request.form['box'])
-        player = request.form['player'].upper().strip()
-        row, col = get_indices(box)
-
-        #retrieve the game state from the form
-        game_data = {
-            'columns': ast.literal_eval(request.form.getlist('columns')[0]),
-            'rows': ast.literal_eval(request.form.getlist('rows')[0]),
-            'answers': ast.literal_eval(request.form.getlist('answers')[0]),
-            'players': ast.literal_eval(request.form.getlist('players')[0]),
-            'xturn': request.form.get('xturn'),
-            'filled_boxes': ast.literal_eval(request.form.getlist('filled_boxes')[0]),
-            'winner': request.form.get('winner')
-        }
-
-        #handle the user's guess for the given box
-        if box not in game_data['filled_boxes']:
-            if player in game_data['players'][box - 1]:
-                game_data['answers'][row][col] = 'X' if game_data['xturn'] == 'True' else 'O'
-                game_data['answers'][row][col] += get_image(player)
-                game_data['filled_boxes'].append(box)
-
-                #check if the game is over or not
-                winner = check_game_status(game_data['answers'])
-                if winner:
-                    game_data['winner'] = winner
-                elif len(game_data['filled_boxes']) >= 9:
-                    game_data['winner'] = 'Draw'
-            #change turns
-            game_data['xturn'] = 'False' if game_data['xturn'] == 'True' else 'True'
-
-
-        print(game_data)
-        return render_template('game.html', game_data=game_data)
-
-
-
+    else:
+        try:
+            box = int(request.form['box'])
+            player = request.form['player'].upper().strip()
+            row, col = get_indices(box)
+            # Defensive: ensure all fields are present and valid
+            def safe_eval(val, default):
+                try:
+                    return ast.literal_eval(val)
+                except Exception as e:
+                    print(f'Error parsing {val}:', e)
+                    return default
+            game_data = {
+                'columns': safe_eval(request.form.get('columns', '[]'), []),
+                'rows': safe_eval(request.form.get('rows', '[]'), []),
+                'answers': safe_eval(request.form.get('answers', '[]'), [['1','2','3'],['4','5','6'],['7','8','9']]),
+                'players': safe_eval(request.form.get('players', '[]'), []),
+                'xturn': request.form.get('xturn'),
+                'filled_boxes': safe_eval(request.form.get('filled_boxes', '[]'), []),
+                'winner': request.form.get('winner')
+            }
+            if box not in game_data['filled_boxes']:
+                if player in game_data['players'][box - 1]:
+                    game_data['answers'][row][col] = 'X' if game_data['xturn'] == 'True' else 'O'
+                    game_data['answers'][row][col] += get_image(player)
+                    game_data['filled_boxes'].append(box)
+                    winner = check_game_status(game_data['answers'])
+                    if winner:
+                        game_data['winner'] = winner
+                    elif len(game_data['filled_boxes']) >= 9:
+                        game_data['winner'] = 'Draw'
+                game_data['xturn'] = 'False' if game_data['xturn'] == 'True' else 'True'
+            # If game is over (win or draw), clear session for new game on Play Again
+            if game_data['winner']:
+                session.pop('columns', None)
+                session.pop('rows', None)
+                session.pop('players', None)
+            return render_template('game.html', game_data=game_data)
+        except Exception as e:
+            print('Error in POST handler:', e)
+            return 'An error occurred while processing your move. Please try again.', 400
 
 if __name__ == '__main__':
     app.run(debug=True)
